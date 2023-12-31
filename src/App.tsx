6cf0c01e139d53965334c56f74b9faf675039b0a
sivaragu <beecesivaragu.com>
2023-12-31 07:39:48 +0530

    modification title

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        Deploy to  Azure  we app
         </a>
       </header>
     </div>
